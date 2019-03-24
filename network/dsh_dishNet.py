c9e78bb53de47960f9926c873a12c908eb5bcf00
lan <lanstarthhh@126.com>
2019-03-24 22:35:23 +0800

    up

--- network/dsh_dishNet.py
+++ network/dsh_dishNet.py
@@ -9,15 +9,15 @@ config = tf.ConfigProto(log_device_placement=True,
                         allow_soft_placement=True)
 
 
-k = 24
+k = 12
 batch_size = 20
 epoch_num = 4
 momentum = 0.9
 weight_decay = 0.004
-base_lr = 0.0001
+base_lr = 0.00001
 m = 2 * k
 alpha = 0.01
-img_size = 64
+img_size = 32
 
 with tf.name_scope("input_image"):
     x = tf.placeholder(tf.float32, shape=[None, img_size, img_size, 3], name="input_image")
