58d59300cf2b8c7b73635cb269aed7a9b5372e18
lan <lanstarthhh@126.com>
2019-04-04 22:24:28 +0800

    up

--- network/dsh_dishNet.py
+++ network/dsh_dishNet.py
@@ -12,10 +12,10 @@ config = tf.ConfigProto(log_device_placement=True,
 
 k = 12
 batch_size = 20
-epoch_num = 15
+epoch_num = 100
 momentum = 0.9
 weight_decay = 0.004
-base_lr = 0.000025
+base_lr = 0.00001
 m = 2 * k
 alpha = 0.01
 img_size = 32
@@ -212,7 +212,7 @@ def main():
 
     global_step = tf.Variable(0, trainable=False)
     with tf.name_scope('lr'):
-        learning_rate = tf.train.exponential_decay(learning_rate=base_lr, global_step=global_step, decay_steps=500, decay_rate=0.4, staircase=True)
+        learning_rate = tf.train.exponential_decay(learning_rate=base_lr, global_step=global_step, decay_steps=2000, decay_rate=0.4, staircase=True)
         tf.summary.scalar('lr', learning_rate)
     #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
     train_step = tf.train.MomentumOptimizer(learning_rate, momentum).minimize(loss, global_step=global_step)
