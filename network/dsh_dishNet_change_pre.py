30008fa94e7b6d04ef8131ac533b8586b8e53cc2
兰诗莹 <lansy@lansydeMBP>
2019-04-09 16:30:49 +0800

    up

--- network/dsh_dishNet_change_pre.py
+++ network/dsh_dishNet_change_pre.py
@@ -11,11 +11,11 @@ config = tf.ConfigProto(log_device_placement=True,
 
 
 k = 8
-batch_size = 50
+batch_size = 25
 epoch_num = 100
 momentum = 0.9
 weight_decay = 0.004
-base_lr = 0.001
+base_lr = 0.0001
 m = 2 * k
 alpha = 0.01
 img_size = 32
@@ -218,8 +218,8 @@ def main():
     with tf.name_scope('lr'):
         learning_rate = tf.train.exponential_decay(learning_rate=base_lr, global_step=global_step, decay_steps=2000, decay_rate=0.4, staircase=True)
         tf.summary.scalar('lr', learning_rate)
-    #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
-    train_step = tf.train.MomentumOptimizer(learning_rate, momentum).minimize(loss, global_step=global_step)
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+    #train_step = tf.train.MomentumOptimizer(learning_rate, momentum).minimize(loss, global_step=global_step)
 
     with tf.Session(config=config) as sess:
 
--- network/dsh_dishNet_change_pre3.py
+++ network/dsh_dishNet_change_pre3.py
@@ -11,7 +11,7 @@ config = tf.ConfigProto(log_device_placement=True,
 
 
 k = 8
-batch_size = 20
+batch_size = 25
 epoch_num = 100
 momentum = 0.9
 weight_decay = 0.004
