52f3bd3858844dba6afd8c6fd390c6e2bb0d3d6b
兰诗莹 <lansy@lansydeMBP>
2019-05-05 23:18:55 +0800

    up

--- network/generate_evaluate_scrip.py
+++ network/generate_evaluate_scrip.py
@@ -1,4 +1,5 @@
 
+'''
 for i in range(432):
     if i <= 146:
         comm = "get%d = `python get_hashcode_v2ep_double.py %d 8`" % (i, i)
@@ -9,5 +10,20 @@ for i in range(432):
     comm1 = "retrieval%d = `python retrieval_v2ep.py %d >> mAP.txt`" % (i, i)
     comm2 = "retrieval%d = `python retrieval_v2ep_hm.py %d >> mAP.txt`" % (i, i)
     with open("run_v4.sh", 'a') as f:
+        f.write(comm + '\n' + comm1 + '\n' + comm2 + '\n')
+'''
+
+
+model_list = [98, 201, 213, 288, 322, 332, 364, 428, 118, 242, 388]
+for i in model_list:
+    if i <= 146:
+        comm = "get%d = `python get_hashcode_v2ep_double_mini.py %d 8`" % (i, i)
+    elif i >= 288:
+        comm = "get%d = `python get_hashcode_v2ep_double_mini.py %d 24`" % (i, i)
+    else:
+        comm = "get%d = `python get_hashcode_v2ep_double_mini.py %d 12`" % (i, i)
+    comm1 = "retrieval%d = `python retrieval_v2ep_mini.py %d >> mAP_mini.txt`" % (i, i)
+    comm2 = "retrieval%d = `python retrieval_v2ep_hm_mini.py %d >> mAP_mini.txt`" % (i, i)
+    with open("run_v5.sh", 'a') as f:
         f.write(comm + '\n' + comm1 + '\n' + comm2 + '\n')
 
---
+++ network/get_hashcode_v2ep_double_mini.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import numpy as np
+import os
+import sys
+#import dsh_dishNet
+
+train_dir = '../data/train_data_mini/'
+test_dir = '../data/test_data_mini/'
+model_dir = './model%s/' % (sys.argv[1])
+batch_size = 100
+output_dir = '../data/output_mini%s/' % (sys.argv[1])
+#model_dir_runtime = '/root/lsy/dishRetrieval/network/model_web/'
+model_dir_runtime = '/Users/lansy/Desktop/graduateDesign/dishRetrieval/network/model_web/'
+img_size = 32
+
+
+
+#x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
+k = int(sys.argv[2])
+
+def prefix_image(image, resize_w, resize_h):
+    image = tf.cast(image, tf.string)
+    image_c = tf.read_file(image)
+    image = tf.image.decode_jpeg(image_c, channels=3)
+
+    image = tf.image.central_crop(image, 0.5)
+    image = tf.image.resize_images(image, [resize_h, resize_w], method=0)
+    image = tf.image.per_image_standardization(image)
+
+    image = tf.cast(image, tf.float32)
+    with tf.Session() as sess:
+        image_numpy = image.eval()
+        image_batch = np.array([image_numpy])
+
+    return image_batch
+
+
+def get_hashcode(image_path):
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(model_dir_runtime + 'model.meta')
+        saver.restore(sess, tf.train.latest_checkpoint(model_dir_runtime))
+        #y_conv = dsh_dishNet.dsh_dish_net(x)
+        y_conv = tf.get_collection('y_conv')[0]
+        graph = tf.get_default_graph()
+        x = graph.get_operation_by_name("input_image/input_image").outputs[0]
+        keep_prob = graph.get_tensor_by_name("Placeholder:0")
+        image = prefix_image(image_path, img_size, img_size)
+        ret = sess.run(y_conv, feed_dict={x: image, keep_prob: 1.0})
+        ret1 = tf.reshape(ret, [k])
+        #ret2 = sess.run(tf.sign(ret1))
+        ret2 = ret1.eval()
+        ret_array = [str(i) for i in ret2]
+        ret_string = ','.join(ret_array)
+    return ret_string
+
+
+def main():
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    #already_get = open(output_dir+'train_output.txt', 'r').read()
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(model_dir+'model-final.meta')
+        saver.restore(sess, tf.train.latest_checkpoint(model_dir))
+        #y_conv = dsh_dishNet.dsh_dish_net(x)
+        y_conv = tf.get_collection('y_conv')[0]
+        graph = tf.get_default_graph()
+        x = graph.get_operation_by_name("input_image/input_image").outputs[0]
+        keep_prob = graph.get_tensor_by_name("Placeholder:0")
+        for label in os.listdir(train_dir):
+            for pic in os.listdir(train_dir + label):
+                image_path = train_dir + label + '/' + pic
+                #if image_path in already_get:
+                    #continue
+                image = prefix_image(image_path, img_size, img_size)
+                ret = sess.run(y_conv, feed_dict={x: image, keep_prob: 1.0})
+                ret1 = tf.reshape(ret, [k])
+                ret2 = sess.run(tf.sign(ret1))
+                ret_array1 = [str(i) for i in ret2]
+                ret_string1 = ','.join(ret_array1)
+                ret3 = ret1.eval()
+                ret_array2 = [str(i) for i in ret3]
+                ret_string2 = ','.join(ret_array2)
+                with open(output_dir+'train_output.txt', 'a') as f1:
+                    f1.write("%s\t%s\t%s\t%s\n" % (image_path, label, ret_string1, ret_string2))
+
+        for pic in os.listdir(test_dir):
+            image_path = test_dir + pic
+            image = prefix_image(image_path, img_size, img_size)
+            ret = sess.run(y_conv, feed_dict={x: image, keep_prob: 1.0})
+            ret1 = tf.reshape(ret, [k])
+            ret2 = sess.run(tf.sign(ret1))
+            ret_array1 = [str(i) for i in ret2]
+            ret3 = ret1.eval()
+            ret_array2 = [str(i) for i in ret3]
+            ret_string2 = ','.join(ret_array2)
+            with open(output_dir+'test_output.txt', 'a') as f1:
+                f1.write("%s\t%s\t%s\n" % (image_path, ','.join(ret_array1), ret_string2))
+
+
+if __name__ == '__main__':
+    main()
+
